2cc468c358f3f0bbc56f7e251961fdf7a52ebeb2
acaptain1993 <aaroncaptain@outlook.com>
2022-07-26 00:11:41 -0400

    Finished both Allure and BrowserStack testing successfully

--- features/environment.py
+++ features/environment.py
@@ -1,15 +1,42 @@
+import allure
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
-#from app.application import Application
+# from app.application import Application
 
 
-def browser_init(context):
+# Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+bs_user = 'aaroncaptain_7Hv5Vj'
+bs_pw = '1pHy8HX5opTQxMduwkVp'
+
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+
+
+def browser_init(context, test_name):
     """
     :param context: Behave context
     """
-    context.driver = webdriver.Chrome(executable_path='C:/Users/aaron/Documents/PythonProjects/python-selenium-automation/chromedriver.exe')
+    # context.driver = webdriver.Chrome(executable_path='C:/Users/aaron/Documents/PythonProjects/python-selenium-automation/chromedriver.exe')
     # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+    # context.browser = webdriver.Firefox(executable_path='C:/Users/aaron/Documents/PythonProjects/gettop-us/geckodriver.exe')
+
+
+
+# Headless Enabled
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('headless')
+    #context.driver = webdriver.Chrome(chrome_options=options, executable_path='C:/Users/aaron/Documents/PythonProjects/python-selenium-automation/chromedriver.exe')
+
+    ### for browerstack ###
+    desired_cap = {
+        "browserName": "chrome",
+        "browserVersion": "103.0",
+        "os": "Windows",
+        "osVersion": "10",
+        'name': test_name
+     }
+    url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
+    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
@@ -19,7 +46,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
